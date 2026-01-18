7dc013c3f9bd3ac1e9be454729bcc20281949389
hassan <hassanmbodraa@gmail.com>
2026-01-18 15:49:05 +0200

    selection sorting

---
+++ selection_sort.py
@@ -0,0 +1,17 @@
+def findSmallest (arr):
+    smallest = arr[0]
+    smallest_index = 0
+    for i in range(1 , len(arr)):
+        if arr[i] < smallest:
+            smallest = arr[i]
+            smallest_index = i
+    return smallest_index
+
+def arr_sort (arr) :
+    newArray = []
+    for i in range(0,len(arr)):
+        smallest = findSmallest(arr)
+        newArray.append(arr.pop(smallest))
+    return newArray
+
+print(arr_sort([31,4,52,6,7]))
